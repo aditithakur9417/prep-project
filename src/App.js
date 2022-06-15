faf046b22baafa41a8b177d4488b00d58f1499fa
Aditi Thakur <aditithakur1425@gmail.com>
2022-06-15 12:55:57 +0530

    prepsaga.com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import React, { useRef } from "react";
 
 function App() {
+
+  function myFunction() {
+    var x = document.getElementById("leftmenuinner");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div>
+  
+</div>
+)
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,69 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import logo from './logo.png';
+import './App.css';
+import React, { useRef } from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+
+  function myFunction() {
+    var x = document.getElementById("leftmenuinner");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  return (
+
+<div className="container-fluid">
+  <div className="row">
+    <div className="col">
+      {/* <img  className="logo" src={logo}/> */}
+    </div>
+    <div className="col">
+      <button className="login">Login </button>
+    </div>
+  </div>
+  <div className="row" id="top">
+    <div className="col">
+
+      <i className='fas fa-home' id="home" ></i>
+    </div>
+    <div className="col">
+      2 of 3
+    </div>
+    <div className="col">
+      3 of 3
+    </div>
+  </div>
+  <div id="leftmenuinner" className="topnav" >
+    <a  target="_top" href="javascript:void(0);" className="icon" onclick={myFunction}>
+      <i className="fa fa-bars"></i>
+    </a>
+    <div id="leftmenuinnerinner" className="box">
+ 
+
+<h2 className="left"><span className="left_h2">CSS</span> Tutorial</h2>
+<a target="_top" href="default.asp" className="active">CSS HOME</a>
+<a target="_top" href="css_intro.asp">CSS Introduction</a>
+<a target="_top" href="css_syntax.asp">CSS Syntax</a>
+<a target="_top" href="css_selectors.asp">CSS Selectors</a>
+<a target="_top" href="css_howto.asp">CSS How To</a>
+<a target="_top" href="css_comments.asp">CSS Comments</a>
+<a target="_top" href="css_colors.asp">CSS Colors</a>
+
+  <a target="_top" href="css_colors.asp">Colors</a>
+  <a target="_top" href="css_colors_rgb.asp">RGB</a>
+  <a target="_top" href="css_colors_hex.asp">HEX</a>
+  <a target="_top" href="css_colors_hsl.asp">HSL</a>
+
+  
+
+    </div>
+    
+  </div>
+</div>
+)
+}
+
+export default App;
